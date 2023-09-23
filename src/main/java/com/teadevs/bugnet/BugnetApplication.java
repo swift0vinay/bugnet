7ca502bc5bef8f17c87c23e21577de321c265ede
Vinay Jain <vinayjain7281@gmail.com>
2023-09-23 18:33:12 +0530

    First Commit

---
+++ src/main/java/com/teadevs/bugnet/BugnetApplication.java
@@ -0,0 +1,13 @@
+package com.teadevs.bugnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugnetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teadevs/bugnet/BugnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teadevs.bugnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
